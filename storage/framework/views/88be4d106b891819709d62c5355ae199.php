1a15e3e7390b2966dfb7b8e9096d7dbd9c1b4f3c
martinhrapco <martinhrapco@gmail.com>
2026-01-23 08:43:56 -0500

    Arreglo en gitignore

--- storage/framework/views/88be4d106b891819709d62c5355ae199.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('contenido'); ?>
-    <section class="container mt-5">
-        <h1 class="text-center">Bienvenido a coldmarket</h1>
-        <p class="text-center">
-            Tecnología, electrodomésticos y soluciones a tu alcance
-        </p>
-    </section>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\User\Herd\proyecto_integrador_ecommerce\resources\views/portada/index.blade.php ENDPATH**/ ?>
